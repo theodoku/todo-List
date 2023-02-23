7d69afa79d844bf49c83a1b292413634810c9c5b
theophilus doku <theodoku89@gmail.com>
2023-02-23 21:40:14 +0000

    modified index.js

--- src/index.js
+++ src/index.js
@@ -18,16 +18,6 @@ const addTodoTask = (todo) => {
   localStorage.setItem('todos', JSON.stringify(todos));
 };
 
-// const removeTodoTask = (index) => {
-//   const todos = getTodoTask();
-//   const deletedTodoTask = todos.filter((todo) => todo.index !== index);
-//   deletedTodoTask.forEach((todo, i) => {
-//     todo.index = i;
-//   });
-//   localStorage.setItem('todos', JSON.stringify(deletedTodoTask));
-//   window.location.reload();
-// };
-
 const updateTodoTask = (index, description) => {
   const todos = getTodoTask();
   const todo = todos.find((todoTask) => todoTask.index === index);
