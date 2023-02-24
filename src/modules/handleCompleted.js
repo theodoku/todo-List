44c90a728e04fa43cfb18c287a35689dd1933149
theophilus doku <theodoku89@gmail.com>
2023-02-24 12:34:51 +0000

    fixed linter for eslint

--- src/modules/handleCompleted.js
+++ src/modules/handleCompleted.js
@@ -1,11 +1,11 @@
 import { getTodo } from './storeTodo.js';
 
 const handleCompleted = (index) => {
-    const completed = document.getElementById(`check${index}`).toggleAttribute('checked');
-    const todos = getTodo();
+  const completed = document.getElementById(`check${index}`).toggleAttribute('checked');
+  const todos = getTodo();
 
-    todos[index].completed = completed;
-    localStorage.setItem('todos', JSON.stringify(todos));
+  todos[index].completed = completed;
+  localStorage.setItem('todos', JSON.stringify(todos));
 };
 
 export default handleCompleted;
